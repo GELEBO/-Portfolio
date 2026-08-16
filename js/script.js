0617d8adb82cc1d64f64354c133b5a755b9c03d2
GELEBO <104772031+GELEBO@users.noreply.github.com>
2026-08-16 08:37:44 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 
+console.log("Portfolio Started!");
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+
+// Open and close mobile menu
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+    if (navLinks.classList.contains("active")) {
+        menuToggle.textContent = "✕";
+    } else {
+        menuToggle.textContent = "☰";
+    }
+
+});
+
+
+// Close menu after clicking a navigation link
+navLinks.querySelectorAll("a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+        menuToggle.textContent = "☰";
+
+    });
+
+});
